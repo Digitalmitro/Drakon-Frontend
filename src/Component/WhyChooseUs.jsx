b3ffe4ab6129c2732d7c0203f89628fbaca6171e
Digitalmitro <digitalmitrous@gmail.com>
2024-05-08 12:44:09 +0530

    mm

--- src/Component/WhyChooseUs.jsx
+++ src/Component/WhyChooseUs.jsx
@@ -1,53 +1,121 @@
 import React from "react";
 import whyChooseUsImg from "../assets/new.png";
 import { useMediaQuery } from "@mui/material";
+import { Flex } from "antd";
 
-const WhyChooseUs = ({closeCart}) => {
-    const isMobile = useMediaQuery("(max-width:500px)");
-    const isTablet = useMediaQuery("(min-width: 500px) and (max-width: 1000px)");
-    console.log(isTablet);
-    return (
-    <div className="container mx-auto py-32 px-10 lg:px-0" onClick={closeCart}>
-      <h2 className="text-center text-2xl lg:text-6xl font-bold mb-10 lg:mb-32">Why Choose Us</h2>
-      <div className="grid grid-cols-3 gap-y-5" style={{zoom: isMobile ? "0.3": isTablet ? "0.5" : ""}}>
-        <div>
-          <h3 className="text-4xl font-bold">01</h3>
-          <h3 className="text-2xl font-bold mb-5">High quality</h3>
-          <p className="text-lg w-[60%] leading-9">
-          Drakon Sports Apparel assures quality materials with breathable and skin-friendly fabrics.
-          </p>
-        </div>
-        <div className="w-[400px] h-[400px] lg-w-full lg:h-full lg:p-10 border-2 border-gray-400 rounded-[50%] flex justify-center items-center">
-          <img
-            src={whyChooseUsImg}
-            alt=""
-            className="rounded-[50%] p-10 border-2 border-gray-400 object-cover"
-          />
-        </div>
-        <div className=" ml-32">
-          <h3 className="text-4xl font-bold">03</h3>
-          <h3 className="text-2xl font-bold mb-5">MONEY BACK GUARANTEE</h3>
-          <p className="text-lg w-[90%] leading-9">
-          We assure you a money-back guarantee if our products do not match your expectations.
-          </p>
-        </div>
-        <div>
-          <h3 className="text-4xl font-bold">02</h3>
-          <h3 className="text-2xl font-bold mb-5">FREE SHIPPING</h3>
-          <p className="text-lg w-[60%] leading-9">
-          Enjoy free shipping on your orders across the United States. Terms and conditions applied.
-          </p>
-        </div>
-        <div></div>
-        <div className=" ml-32">
-          <h3 className="text-4xl font-bold">04</h3>
-          <h3 className="text-2xl font-bold mb-5">PROFESSIONAL SUPPORT</h3>
-          <p className="text-lg w-[90%] leading-9">
-           Have questions and complaints regarding your orders? Reach out to our customer support team.
-          </p>
+const WhyChooseUs = ({ closeCart }) => {
+  const isMobile = useMediaQuery("(max-width:500px)");
+  const isTablet = useMediaQuery("(min-width: 500px) and (max-width: 1000px)");
+  console.log(isTablet);
+  return (
+    <>
+      <div
+        className="container mx-auto py-32 px-10 lg:px-0 hidden lg:block"
+        onClick={closeCart}
+      >
+        <h2 className="text-center text-2xl lg:text-6xl font-bold mb-10 lg:mb-32">
+          Why Choose Us
+        </h2>
+        <div
+          className="grid grid-cols-3 gap-y-5"
+          style={{ zoom: isMobile ? "0.3" : isTablet ? "0.5" : "" }}
+        >
+          <div>
+            <h3 className="text-4xl font-bold">01</h3>
+            <h3 className="text-2xl font-bold mb-5">High quality</h3>
+            <p className="text-lg w-[60%] leading-9">
+              Drakon Sports Apparel assures quality materials with breathable
+              and skin-friendly fabrics.
+            </p>
+          </div>
+          <div className="w-[400px] h-[400px] lg-w-full lg:h-full lg:p-10 border-2 border-gray-400 rounded-[50%] flex justify-center items-center">
+            <img
+              src={whyChooseUsImg}
+              alt=""
+              className="rounded-[50%] p-10 border-2 border-gray-400 object-cover"
+            />
+          </div>
+          <div className=" ml-32">
+            <h3 className="text-4xl font-bold">03</h3>
+            <h3 className="text-2xl font-bold mb-5">MONEY BACK GUARANTEE</h3>
+            <p className="text-lg w-[90%] leading-9">
+              We assure you a money-back guarantee if our products do not match
+              your expectations.
+            </p>
+          </div>
+          <div>
+            <h3 className="text-4xl font-bold">02</h3>
+            <h3 className="text-2xl font-bold mb-5">FREE SHIPPING</h3>
+            <p className="text-lg w-[60%] leading-9">
+              Enjoy free shipping on your orders across the United States. Terms
+              and conditions applied.
+            </p>
+          </div>
+          <div></div>
+          <div className=" ml-32">
+            <h3 className="text-4xl font-bold">04</h3>
+            <h3 className="text-2xl font-bold mb-5">PROFESSIONAL SUPPORT</h3>
+            <p className="text-lg w-[90%] leading-9">
+              Have questions and complaints regarding your orders? Reach out to
+              our customer support team.
+            </p>
+          </div>
         </div>
       </div>
-    </div>
+
+      <div
+        className="container mx-auto py-32 px-10 lg:px-0 lg:hidden"
+        onClick={closeCart}
+      >
+        <h2 className="text-center text-5xl lg:text-6xl font-bold mb-10 lg:mb-32">
+          Why Choose Us
+        </h2>
+        <div className="grid  gap-y-5">
+          <div className="w-full h-[450px] lg-w-full lg:h-full lg:p-10 border-2 border-gray-400 rounded-[50%] flex justify-center items-center">
+            <img
+              src={whyChooseUsImg}
+              alt=""
+              className="rounded-[50%] p-10 border-2 border-gray-400 object-cover"
+            />
+          </div>
+         
+          <div style={{display:"flex",flexDirection:"column" , justifyContent:'center', alignItems:"center", alignContent:'center'}} className="mt-[70px]  justify-center mb-[25px]">
+            <h3 className="text-4xl  font-bold">01</h3>
+            <h3 className="text-2xl font-bold mb-5">High quality</h3>
+            <p className="text-lg w-[60%] leading-9 text-center">
+              Drakon Sports Apparel assures quality materials with breathable
+              and skin-friendly fabrics.
+            </p>
+          </div>
+          <div  style={{display:"flex",flexDirection:"column" , justifyContent:'center', alignItems:"center", alignContent:'center'}} className="mb-[25px]">
+            <h3 className="text-4xl font-bold">02</h3>
+            <h3 className="text-2xl font-bold mb-5">FREE SHIPPING</h3>
+            <p className="text-lg w-[60%] leading-9 text-center">
+              Enjoy free shipping on your orders across the United States. Terms
+              and conditions applied.
+            </p>
+          </div>
+          <div style={{display:"flex",flexDirection:"column" , justifyContent:'center', alignItems:"center", alignContent:'center'}} className="mb-[25px] ">
+            <h3 className="text-4xl font-bold">03</h3>
+            <h3 className="text-2xl font-bold mb-5">MONEY BACK GUARANTEE</h3>
+            <p className="text-lg w-[90%] leading-9 text-center">
+              We assure you a money-back guarantee if our products do not match
+              your expectations.
+            </p>
+          </div>
+          <div style={{display:"flex",flexDirection:"column" , justifyContent:'center', alignItems:"center", alignContent:'center'}} className="mb-[25px]">
+            <h3 className="text-4xl font-bold">04</h3>
+            <h3 className="text-2xl font-bold mb-5">PROFESSIONAL SUPPORT</h3>
+            <p className="text-lg w-[90%] leading-9 text-center">
+              Have questions and complaints regarding your orders? Reach out to
+              our customer support team.
+            </p>
+          </div>
+       
+         
+        </div>
+      </div>
+    </>
   );
 };
 
