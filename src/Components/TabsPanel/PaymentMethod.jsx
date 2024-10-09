d2e515b5fa633e3646c94203d32a4ac974e27b19
digitalmitro <digitalmitrous@gmail.com>
2024-10-09 17:04:34 +0530

    address list , drawer

--- src/Components/TabsPanel/PaymentMethod.jsx
+++ src/Components/TabsPanel/PaymentMethod.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import "../Styles/AddressPanel.scss"
+import "../Styles/PaymentPanel.scss"
 import styles from "../styles/payment.module.scss"
 export const PaymentPanel = () => {
   const navigate = useNavigate();
