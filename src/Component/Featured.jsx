2a6a331c110d3995f5668388e9f9b06f4829df45
digitalmitro <digitalmitrous@gmail.com>
2025-03-17 18:37:41 +0530

    product section made

--- src/Component/Featured.jsx
+++ src/Component/Featured.jsx
@@ -1,75 +1,119 @@
 import { Button } from "@mui/material";
-import React, { useEffect, useState } from "react";
-import tshirt from "../assets/tshirt.png";
-import cap from "../assets/cap.png";
-import sunglassNew from "../assets/sunglass-new.png";
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { addItem } from "../Redux/CartSlice";
-import { message } from "antd";
-import feature from "../feature.json"
-import { Link } from "react-router-dom";
-const Featured = ({closeCart}) => {
-  const dispatch = useDispatch();
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
 
+import glass1 from "../assets/glasses/glass1.jpg";
+import glass2 from "../assets/glasses/glass2.jpg";
+import glass3 from "../assets/glasses/glass3.jpg";
+import glass4 from "../assets/glasses/glass4.jpg";
+import { useEffect } from "react";
+const glassesProduct = [
+  {
+    img: glass1,
+    title: "Blue glasses",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: "99",
+    id: 1,
+  },
+  {
+    img: glass2,
+    title: "Pink glasses",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: "150",
+    id: 2,
+  },
+  {
+    img: glass3,
+    title: "Black glasses",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+    price: "122",
+    id: 3,
+  },
+];
+const Featured = ({ closeCart, navigate }) => {
+  const [glass, setGlass] = useState([]);
+  const getAllProducts = async () => {
+    const response = await fetch(
+      `${import.meta.env.VITE_BACKEND_API}/products`
+    );
+    if (response.ok) {
+      const data = await response.json();
+      console.log("show all products", data);
+      setGlass(data);
+    }
+  };
   useEffect(() => {
-    getProducts();
+    getAllProducts();
   }, []);
 
-  const [products, setProducts] = useState([]);
-  async function getProducts() {
-    try {
-      const { data } = await axios.get(
-        `${import.meta.env.VITE_BACKEND_API}/feature-products`
-      );
-      setProducts(data);
-    } catch (error) {
-      console.log(error);
-    }
-  }
   return (
     <div className="bg-[#F3F3F3]" onClick={closeCart}>
-      <div className="container mx-auto" style={{}}>
+      <div className=" mx-auto py-40">
         <h2 className="font-bold text-4xl lg:text-5xl uppercase text-center">
           Featured Products
         </h2>
-        <div className="flex flex-col lg:flex-row gap-10 justify-between py-20">
-          {feature?.map((e) => (
-           <Link to={`/productDetails/${e._id}`}>
-             <div className="shadow-lg lg:h-[600px] lg:w-[400px] lg:p-20 flex flex-col justify-between items-center gap-6 lg:gap-0 bg-white p-10">
-              <div className="lg:w-[200px] h-[200px] flex justify-center">
-                <img
-                  src={e.image}
-                  className="object-cover w-1/2 lg:w-full"
-                  style={{}}
-                />
-              </div>
-              <h3 className="font-bold text-3xl">{e.title}</h3>
-              <h4 className="text-[#959595] font-bold text-2xl">$ {e.price}</h4>
-              <Button
-                // onClick={() => {
-                //   dispatch(addItem(e))
-                //   message.success("Item added to cart")
-                // }}
-                sx={{
-                  borderRadius: "100vw",
-                  paddingY: "10px",
-                  fontSize: "1rem",
-                  width: "80%",
-                  backgroundColor: "#F5743B",
-                  "&:hover": {
-                    backgroundColor: "#be410c", // Adjust the brightness to darken the color
-                  },
-                }}
-                variant="contained"
-                onClick={() => navigate(`/checkout/${id}`)}
-              >
-                Buy now
-              </Button>
-            </div>
-           </Link>
+
+        <Swiper
+          slidesPerView={3}
+          spaceBetween={20}
+          loop={true}
+          pagination={{ clickable: true }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          breakpoints={{
+            1024: { slidesPerView: 3 },
+            600: { slidesPerView: 2 },
+            375: { slidesPerView: 1 },
+          }}
+          className="mt-10 lg:ml-20"
+        >
+          {glass.map((e) => (
+            <SwiperSlide key={e._id}>
+              <Link to={`/productDetails/${e._id}`}>
+                <div className="shadow-lg lg:h-[520px] rounded lg:w-[400px] flex flex-col justify-between items-center gap-6 bg-white p-2 ">
+                  <div className="flex justify-center">
+                    <img
+                      src={e.image?.[0]}
+                      className="object-contain w-1/2 lg:w-full lg:h-[250px]"
+                      alt="Product"
+                    />
+                  </div>
+                  <div className="p-10 h-full space-y-4">
+                    <h3 className="font-semibold text-xl text-center lg:h-[60px]">
+                      {e.description.length > 60
+                        ? `${e.description.slice(0, 55)}...`
+                        : e.description}
+                    </h3>
+                    <h4 className="text-[#959595] font-bold text-2xl text-center">
+                      $ {e.price}
+                    </h4>
+                    <div className="flex justify-center lg:w-[300px]">
+                      <Button
+                        sx={{
+                          borderRadius: "100vw",
+                          paddingY: "10px",
+                          fontSize: "1rem",
+                          width: "80%",
+                          backgroundColor: "#ff5B00",
+                          "&:hover": { backgroundColor: "#ff0024" },
+                        }}
+                        variant="contained"
+                        onClick={() => navigate(`/checkout/${e.id}`)}
+                      >
+                        Buy now
+                      </Button>
+                    </div>
+                  </div>
+                </div>
+              </Link>
+            </SwiperSlide>
           ))}
-        </div>
+        </Swiper>
       </div>
     </div>
   );
--- src/Component/Hero.jsx
+++ src/Component/Hero.jsx
@@ -10,6 +10,7 @@ import { motion } from "framer-motion";
 import carousel1 from "../assets/carousel/crousal-1.jpg";
 import carousel2 from "../assets/carousel/crousal-2.webp";
 import carousel3 from "../assets/carousel/crousal-3.jpg";
+// import carousel4 from "../assets/carousel/crousal-4.jpg";
 const Hero = ({ closeCart }) => {
   const navigate = useNavigate();
   const isMobile = useMediaQuery("(max-width:900px)");
@@ -35,10 +36,11 @@ const Hero = ({ closeCart }) => {
                   <h3 className="uppercase text-xl lg:text-4xl font-semibold mb-10 w-[600px]">
                     <span className="text-amber-500 text-lg ">
                       NEW NEXT SERIES
-                    </span><br />
+                    </span>
+                    <br />
                     <span>PERFECT GLOVES FOR TRAVEL BALL PLAYERS</span>
                   </h3>
-                  <div >
+                  <div>
                     <Button
                       sx={{
                         borderRadius: "100vw",
@@ -46,117 +48,23 @@ const Hero = ({ closeCart }) => {
                         fontSize: "1rem",
                         fontWeight: "bold",
                         backgroundColor: "#F5743B",
-                        
+
                         mr: "20px",
                         "&:hover": { backgroundColor: "#be410c" },
                       }}
                       variant="contained"
                       onClick={() => navigate("/product")}
                     >
-                      Shop Now 
+                      Shop Now
                     </Button>
-
-                    {/* <Button
-                      sx={{
-                        borderRadius: "100vw",
-                        padding: "15px 40px",
-                        fontSize: "1rem",
-                        fontWeight: "bold",
-                        backgroundColor: "white",
-                        color: "black",
-                        "&:hover": { backgroundColor: "#cccccc" },
-                      }}
-                      variant="contained"
-                      onClick={() => navigate("/product")}
-                    >
-                      Shop Women
-                    </Button> */}
                   </div>
                 </div>
               </div>
             </div>
           ))}
         </Carousel>
-        <div
-        // className="hero-bg-img flex items-center  bg-cover h-[700px]  lg:bg-cover"
-        // style={{ zoom: isMobile ? "0.7" : "" }}
-        >
-          {/* <div className=" ml-[20px] lg:ml-[200px] text-center lg:text-left">
-            <h3 className="uppercase text-3xl lg:text-5xl font-bold text-white mb-10">
-              Always in motion
-            </h3>
-            <Button
-              sx={{
-                borderRadius: "100vw",
-                padding: "15px 40px",
-                fontSize: "1rem",
-                fontWeight: "bold",
-                backgroundColor: "#F5743B",
-                ml: isMobile ? "" : "40px",
-                mr: "20px",
-                "&:hover": {
-                  backgroundColor: "#be410c", 
-                },
-                zoom: isMobile ? "0.7" : ""
-              }}
-              variant="contained"
-              onClick={() => navigate('/product')}
-            >
-              shop men
-            </Button>
-
-            <Button
-              sx={{
-                borderRadius: "100vw",
-                padding: "15px 40px",
-                fontSize: "1rem",
-                fontWeight: "bold",
-                backgroundColor: "white",
-                color: "black",
-                "&:hover": {
-                  backgroundColor: "#cccccc", // Adjust the brightness to darken the color
-                },
-                zoom: isMobile ? "0.7" : ""
-              }}
-              variant="contained"
-              onClick={() => navigate('/product')}
-            >
-              shop women
-            </Button>
-          </div> */}
-        </div>
-
-        <div className="container mx-auto py-10 grid lg:grid-cols-2 gap-10">
-          {/* <div className="w-full flex flex-col items-center justify-center">
-            <img src={gloves} alt="" className="lg:w-64" />
-            <h3 className="text-4xl lg:text-3xl font-bold">Cricket Gloves</h3>
-            <h3 className="text-3xl lg:text-2xl font-bold my-5">$ 125</h3>
-            <p className="text-xl lg:text-lg">Drakon Sports Apparel gloves...</p>
-          </div> */}
-          {/* <div className="w-full flex flex-col items-center justify-center">
-            <img src={elbowGuard} alt="" className="lg:w-64" />
-            <h3 className="text-4xl lg:text-3xl font-bold">Elbow Guard</h3>
-            <h3 className="text-3xl lg:text-2xl font-bold my-5">$ 125</h3>
-            <p className="text-xl lg:text-lg">Drakon Sports elbow guard...</p>
-          </div> */}
-          <Link to={"/product"}>
-            <div className="w-full flex flex-col items-center justify-center">
-              <img src={tshirt} alt="" className="lg:mb-[0px] lg:h-[400px]" />
-              <h3 className="text-4xl lg:text-3xl font-bold">Drakon Hoodie</h3>
-              <h3 className="text-3xl lg:text-2xl font-bold my-3">$ 125</h3>
-              <p className="text-xl lg:text-lg">
-                Drakon Sports Apparel hoodie...
-              </p>
-            </div>
-          </Link>
-          <Link to={"/product"}>
-            <div className="w-full flex flex-col items-center justify-center">
-              <img src={sunglass} alt="" className="lg:w-64" />
-              <h3 className="text-4xl lg:text-3xl font-bold">Sunglass</h3>
-              <h3 className="text-3xl lg:text-2xl font-bold my-5">$ 125</h3>
-              <p className="text-xl lg:text-lg">Drakon Sports sunglass...</p>
-            </div>
-          </Link>
+        <div>
+          {/* <img src={"carousel4"} alt="" /> */}
         </div>
       </div>
     </>
