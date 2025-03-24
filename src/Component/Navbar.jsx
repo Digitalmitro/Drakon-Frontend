5e81a5077b1e01d3b5afe14703878df49c100cf3
digitalmitro <digitalmitrous@gmail.com>
2025-03-24 11:50:39 +0530

    added

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -169,56 +169,56 @@ function Navbar(props) {
             className="w-36 cursor-pointer logoMobile"
           />
 
-          <div className="flex flex-col lg:justify-end justify-center items-center lg:items-end">
-            <div className="text-black lg:pt-6 flex lg:space-x-12 space-x-5">
-              <div className="flex bg-white rounded-lg">
-                <input
-                  type="text"
-                  className="outline-none border-none px-2 py-1 rounded-lg"
-                  placeholder="Search..."
-                />
-                <img src={search} alt="" className="h-8 p-1" />
-              </div>
+            <div className="flex flex-col lg:justify-end justify-center items-center lg:items-end">
+              <div className="text-black lg:pt-6 flex lg:gap-12 gap-6">
+                <div className="flex bg-white rounded-lg">
+                  <input
+                    type="text"
+                    className="outline-none border-none px-2 py-1 rounded-lg"
+                    placeholder="Search..."
+                  />
+                  <img src={search} alt="" className="h-8 p-1" />
+                </div>
 
-              <div
-                className="flex justify-center items-center cursor-pointer"
-                onClick={() => navigate("/account")}
-              >
-                <p className="hidden lg:block">My Accounts</p>{" "}
-                <img src={profile} alt="" className="h-8 p-1 pb-2 block" />
+                <div
+                  className="flex justify-center items-center cursor-pointer"
+                  onClick={() => navigate("/account")}
+                >
+                  <p className="hidden lg:block">My Accounts</p>{" "}
+                  <img src={profile} alt="" className="h-8 p-1 pb-2 block" />
+                </div>
+                <div
+                  className="flex justify-center space-x-1 items-center cursor-pointer"
+                  onClick={() => navigate("/cart")}
+                >
+                  <p className="hidden lg:block">Cart</p>
+                  <IoCart size={22} />
+                </div>
               </div>
-              <div
-                className="flex justify-center space-x-1 items-center cursor-pointer"
-                onClick={() => navigate("/cart")}
-              >
-                <p className="hidden lg:block">Cart</p>
-                <IoCart size={22} />
-              </div>
+              <Box>
+                <div>
+                  <List sx={{ display: { xs: "none", sm: "block" } }}>
+                    <ListItem disablePadding>
+                      {navItems.map((item) => (
+                        <ListItemButton
+                          key={item.name}
+                          sx={{
+                            textAlign: "center",
+                            "&:hover": { backgroundColor: "transparent" },
+                          }}
+                          onClick={() => navigate(item.path)}
+                        >
+                          <ListItemText
+                            primary={item.name}
+                            className="text-black hover:transition hover:scale-105 hover:-translate-y-1 hover:delay-100 hover:ease-in-out hover:duration-300 hover:text-[#0024ff]"
+                          />
+                        </ListItemButton>
+                      ))}
+                    </ListItem>
+                  </List>
+                </div>
+              </Box>
             </div>
-            <Box>
-              <div>
-                <List sx={{ display: { xs: "none", sm: "block" } }}>
-                  <ListItem disablePadding>
-                    {navItems.map((item) => (
-                      <ListItemButton
-                        key={item.name}
-                        sx={{
-                          textAlign: "center",
-                          "&:hover": { backgroundColor: "transparent" },
-                        }}
-                        onClick={() => navigate(item.path)}
-                      >
-                        <ListItemText
-                          primary={item.name}
-                          className="text-black hover:transition hover:scale-105 hover:-translate-y-1 hover:delay-100 hover:ease-in-out hover:duration-300 hover:text-[#0024ff]"
-                        />
-                      </ListItemButton>
-                    ))}
-                  </ListItem>
-                </List>
-              </div>
-            </Box>
-          </div>
         </Toolbar>
       </AppBar>
       <nav>
