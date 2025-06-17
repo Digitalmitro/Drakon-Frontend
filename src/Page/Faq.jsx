22b1f05f52314fa31bf0fd047c1420a72421be0b
Twinkle0901 <twinkle.digitalmitro@gmail.com>
2025-06-17 17:44:02 +0530

    added

--- src/Page/Faq.jsx
+++ src/Page/Faq.jsx
@@ -1,99 +1,206 @@
 const Faq = () => {
+  return (
+    <>
+      <section>
+        <div className="container-fluid faq-banner">
+          <h2 className="text-center size">faq</h2>
+        </div>
+      </section>
 
-    return (
-        <>
-
-            <section >
-                <div className="container-fluid faq-banner">
-
-                    <h2 className="text-center size">faq</h2>
+      <section>
+        <div className="container">
+          <div className="mb-8">
+            <div className="container">
+              <div className="row justify-content-center">
+                <div className="col-11 col-xl-10 my-5">
+                  <h3
+                    className="m-0 fs-1 text "
+                    style={{ color: "#ff6702", textAlign: "center" }}
+                  >
+                    Frequently Asked Questions
+                  </h3>
+                  <p className="fs-4 text text-center py-3">
+                    Answered all Frequently Asked Questions. Still confused?{" "}
+                    <br />
+                    feel free to Contact us
+                  </p>
                 </div>
-            </section>
 
-            <section>
-                <div className="container">
-                    <div className="mb-8">
-                        <div className="container">
-                            <div className="row justify-content-center">
-                                <div className="col-11 col-xl-10 my-5">
-                                        <h3 className="m-0 fs-1 text " style={{color:"#ff6702",textAlign:"center"}}>Frequently Asked Questions</h3>
-                                        <p className="fs-4 text text-center py-3">Answered all Frequently Asked Questions. Still confused? <br/>feel free to Contact us</p>
-                                    </div>
-                              
-                                <div className="col-12">
-                                    <div className="accordion accordion-flush" id="faqAccount">
-                                        <div className="accordion-item bg-transparent border-top border-bottom py-3">
-                                            <h2 className="accordion-header" id="faqAccountHeading1">
-                                                <button className="accordion-button collapsed bg-transparent fs-4 text " type="button" data-bs-toggle="collapse" data-bs-target="#faqAccountCollapse1" aria-expanded="false" aria-controls="faqAccountCollapse1">
-                                                    What is an account?
-                                                </button>
-                                            </h2>
-                                            <div id="faqAccountCollapse1" className="accordion-collapse collapse" aria-labelledby="faqAccountHeading1">
-                                                <div className="accordion-body">
-                                                    <p>An account is a personal or organizational record that allows you to access and manage various services, often requiring authentication through a username and password.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="accordion-item bg-transparent border-bottom py-3">
-                                            <h2 className="accordion-header" id="faqAccountHeading2">
-                                                <button className="accordion-button collapsed bg-transparent fs-4 text" type="button" data-bs-toggle="collapse" data-bs-target="#faqAccountCollapse2" aria-expanded="false" aria-controls="faqAccountCollapse2">
-                                                    How do I create an account?
-                                                </button>
-                                            </h2>
-                                            <div id="faqAccountCollapse2" className="accordion-collapse collapse" aria-labelledby="faqAccountHeading2">
-                                                <div className="accordion-body">
-                                                    <p>To create an account, visit the website or application and look for a "Sign Up" or "Create Account" button. Follow the instructions to enter your information and create a username and password.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="accordion-item bg-transparent border-bottom py-3">
-                                            <h2 className="accordion-header" id="faqAccountHeading3">
-                                                <button className="accordion-button collapsed bg-transparent fs-4 text" type="button" data-bs-toggle="collapse" data-bs-target="#faqAccountCollapse3" aria-expanded="false" aria-controls="faqAccountCollapse3">
-                                                    How do I secure my account?
-                                                </button>
-                                            </h2>
-                                            <div id="faqAccountCollapse3" className="accordion-collapse collapse" aria-labelledby="faqAccountHeading3">
-                                                <div className="accordion-body">
-                                                    <p>To secure your account, use a robust and unique password, enable two-factor authentication if available, and be cautious about sharing your login information. Regularly update your password and avoid using easily guessable information like birthdays or names.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="accordion-item bg-transparent border-bottom py-3">
-                                            <h2 className="accordion-header" id="faqAccountHeading4">
-                                                <button className="accordion-button collapsed bg-transparent fs-4 text" type="button" data-bs-toggle="collapse" data-bs-target="#faqAccountCollapse4" aria-expanded="false" aria-controls="faqAccountCollapse4">
-                                                    What should I do if I forget my password?
-                                                </button>
-                                            </h2>
-                                            <div id="faqAccountCollapse4" className="accordion-collapse collapse" aria-labelledby="faqAccountHeading4">
-                                                <div className="accordion-body">
-                                                    <p>If you forget your password, most websites and services offer a "Forgot Password" or "Reset Password" option. Follow the steps to reset your password, often involving an email or SMS verification.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className="accordion-item bg-transparent border-bottom py-3">
-                                            <h2 className="accordion-header" id="faqAccountHeading5">
-                                                <button className="accordion-button collapsed bg-transparent fs-4 text" type="button" data-bs-toggle="collapse" data-bs-target="#faqAccountCollapse5" aria-expanded="false" aria-controls="faqAccountCollapse5">
-                                                    What should I do if my account is compromised or hacked?
-                                                </button>
-                                            </h2>
-                                            <div id="faqAccountCollapse5" className="accordion-collapse collapse" aria-labelledby="faqAccountHeading5">
-                                                <div className="accordion-body">
-                                                    <p>If you suspect your account has been compromised, change your password immediately. Contact the service provider for further assistance, and consider enabling two-factor authentication for added security.</p>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                <div className="col-12">
+                  <div className="accordion accordion-flush" id="faqAccount">
+                    <div className="accordion-item bg-transparent border-top border-bottom py-3">
+                      <h2 className="accordion-header" id="faqAccountHeading1">
+                        <button
+                          className="accordion-button collapsed bg-transparent fs-4 text "
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#faqAccountCollapse1"
+                          aria-expanded="false"
+                          aria-controls="faqAccountCollapse1"
+                        >
+                          What is the equipment used for baseball?
+                        </button>
+                      </h2>
+                      <div
+                        id="faqAccountCollapse1"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="faqAccountHeading1"
+                      >
+                        <div className="accordion-body">
+                          <p>
+                            There are several pieces of equipment, like batting
+                            gloves, sunglasses, sliding mitt, and more that are
+                            used as equipment for baseball. At Drakon Sports, we
+                            provide all the essential equipment required to
+                            strengthen your baseball game. However, our core
+                            lineup focuses on performance-enhancing apparel. We
+                            also collaborate with traditional baseball equipment
+                            to ensure athletes are fully game-ready.
+                          </p>
                         </div>
+                      </div>
                     </div>
+                    <div className="accordion-item bg-transparent border-bottom py-3">
+                      <h2 className="accordion-header" id="faqAccountHeading2">
+                        <button
+                          className="accordion-button collapsed bg-transparent fs-4 text"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#faqAccountCollapse2"
+                          aria-expanded="false"
+                          aria-controls="faqAccountCollapse2"
+                        >
+                          2. What is the glove they wear in baseball?
+                        </button>
+                      </h2>
+                      <div
+                        id="faqAccountCollapse2"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="faqAccountHeading2"
+                      >
+                        <div className="accordion-body">
+                          <p>
+                            The baseball gloves are necessary for players,
+                            offering both protection and performance
+                            enhancement. So, at Drakon Sports, you will find
+                            quality gloves that are large and have a leather
+                            mitt with a deep pocket. Additionally, our premium
+                            glove accessories, like leather batting gloves, are
+                            designed to enhance grip and comfort for players. We
+                            support athletes who already use regulation fielding
+                            gloves alongside our products.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="accordion-item bg-transparent border-bottom py-3">
+                      <h2 className="accordion-header" id="faqAccountHeading3">
+                        <button
+                          className="accordion-button collapsed bg-transparent fs-4 text"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#faqAccountCollapse3"
+                          aria-expanded="false"
+                          aria-controls="faqAccountCollapse3"
+                        >
+                          3. What kind of sunglasses do baseball players wear?
+                        </button>
+                      </h2>
+                      <div
+                        id="faqAccountCollapse3"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="faqAccountHeading3"
+                      >
+                        <div className="accordion-body">
+                          <p>
+                            Sunglasses are one of the most essential accessories
+                            for baseball players because they not only protect
+                            eyes from UV rays but also improve visibility. At
+                            Drakon Sports, our sunglasses are especially
+                            engineered for baseball players and athletes in
+                            general. You will get access to blue and black
+                            frames to boost the look, and have clarity, blocked
+                            glare, and effective visuality on the field. The
+                            sleek, durable design makes them the top choice
+                            under bright stadium lights.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="accordion-item bg-transparent border-bottom py-3">
+                      <h2 className="accordion-header" id="faqAccountHeading4">
+                        <button
+                          className="accordion-button collapsed bg-transparent fs-4 text"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#faqAccountCollapse4"
+                          aria-expanded="false"
+                          aria-controls="faqAccountCollapse4"
+                        >
+                          4. What are some baseball accessories?
+                        </button>
+                      </h2>
+                      <div
+                        id="faqAccountCollapse4"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="faqAccountHeading4"
+                      >
+                        <div className="accordion-body">
+                          <p>
+                            The baseball accessories include a wide variety of
+                            items that enhance gameplay, comfort, and safety.
+                            The accessories range from training and convenience
+                            such as batting tees and baseball bags to protective
+                            gears like batting helmets and gloves. Enhance grip
+                            and hand protection with leather batting gloves,
+                            reduce glare and sharpen vision with performance
+                            glasses. Additionally, support your muscles and have
+                            faster recovery with compression arm sleeves. We
+                            have each product right at our online store.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="accordion-item bg-transparent border-bottom py-3">
+                      <h2 className="accordion-header" id="faqAccountHeading5">
+                        <button
+                          className="accordion-button collapsed bg-transparent fs-4 text"
+                          type="button"
+                          data-bs-toggle="collapse"
+                          data-bs-target="#faqAccountCollapse5"
+                          aria-expanded="false"
+                          aria-controls="faqAccountCollapse5"
+                        >
+                          5. What do baseball players wear on their elbows?
+                        </button>
+                      </h2>
+                      <div
+                        id="faqAccountCollapse5"
+                        className="accordion-collapse collapse"
+                        aria-labelledby="faqAccountHeading5"
+                      >
+                        <div className="accordion-body">
+                          <p>
+                            Baseball players often utilize compression arm
+                            sleeves to support and protect their elbows. The arm
+                            sleeve at Drakon Sports, is specially built to
+                            reduce fatigue, help stabilize muscles during
+                            pitching or fielding and speed recovery afterwards.
+                            Our sleeves are breathable, ergonomic, and specially
+                            designed to enhance athletic performance from
+                            warm-up through the last pitch.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-            </section>
-
-
-
-
-        </>
-    )
-}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 export default Faq;
